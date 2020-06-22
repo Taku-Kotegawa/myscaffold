305f63f1a39aef0f5d059d33e452e1a5d2da7d08
taku.kotegawa <taku.kotegawa@gmail.com>
2020-06-23 00:19:11 +0900

    コードクリーン

--- myscaffold-web/src/main/java/com/example/app/account/AccountCreateForm.java
+++ myscaffold-web/src/main/java/com/example/app/account/AccountCreateForm.java
@@ -15,42 +15,48 @@ import java.io.Serializable;
 public class AccountCreateForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    ;
+
     @NotNull                    //null禁止
     @NotContainControlChars     //制御文字禁止
     @Size(min = 4, max = 128)   //4文字以上128文字以下
     private String username;
-    ;
+
     @NotNull
     @NotContainControlChars
     @Size(min = 1, max = 128)
     private String firstName;
+
     @NotNull
     @NotContainControlChars
     @Size(min = 1, max = 128)
     private String lastName;
+
     @NotNull
     @NotContainControlChars
     @Size(min = 1, max = 128)
-    @DomainRestrictedEmail(allowedDomains = {"domainexample.co.jp",
-            "somedomainexample.co.jp"}, allowSubDomain = true)
+    @DomainRestrictedEmail(allowedDomains = {"domainexample.co.jp", "somedomainexample.co.jp"}, allowSubDomain = true)
     private String email;
+
     @NotNull
     @NotContainControlChars
     private String confirmEmail;
+
     @NotNull
     @NotContainControlChars
     @DomainRestrictedURL(allowedDomains = {"jp"})
     private String url;
+
     @UploadFileRequired(groups = Confirm.class)
     @UploadFileNotEmpty(groups = Confirm.class)
     @UploadFileMaxSize
     @FileExtension(extensions = {"jpg", "png", "gif"})
     @FileNamePattern(pattern = "[a-zA-Z0-9_-]+\\.[a-zA-Z]{3}")
     private MultipartFile image;
+
     @NotNull(groups = CreateAccount.class)
     @Size(max = 40)
     private String imageId;
+
     @NotNull
     @NotContainControlCharsExceptNewlines
     private String profile;
